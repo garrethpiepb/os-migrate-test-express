4c688746e2278b790a6e72e2e17de3046dd49517
Garreth Piekarski <garreth.piekarski@pb.com>
2020-07-17 14:08:02 -0400

    port change oops

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 
 app.use(express.static('static'));
